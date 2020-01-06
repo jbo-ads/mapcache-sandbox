9f1c84863e81462fdd524a8c755b4a1daf3b3f4a
jbo-ads <jbo-ads@laposte.net>
2020-01-06 16:02:53 +0100

    Suite de la réorganisation

--- bin/setup-mapcache.sh
+++ bin/setup-mapcache.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-bindir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+bindir=/share/bin
+if [ ! -e ${bindir} ]
+then
+  bindir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+fi
 
 sed -i '/^LogLevel/s/ mapcache:[a-z0-9]*//' /etc/apache2/apache2.conf
 sed -i '/^LogLevel/s/$/ mapcache:debug/' /etc/apache2/apache2.conf
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-bindir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+bindir=/share/bin
+if [ ! -e ${bindir} ]
+then
+  bindir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+fi
 
 for component in mapcache apache
 do
